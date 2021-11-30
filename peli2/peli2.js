3aafa4aa6dd5617ee24107699bc87ae87bb14f37
Petri M <petri.malmstrom@gmail.com>
2021-11-30 11:18:29 +0200

    peli2 valmis

--- peli2/peli2.js
+++ peli2/peli2.js
@@ -1,13 +1,28 @@
-const dino = document.getElementsByClassName('dino');
-const rock = document.getElementsByClassName('rock');
-const score = document.getElementsByClassName('score');
+const dino = document.getElementById('saurus');
+const rock = document.getElementById('kivi');
+// const score = document.getElementById('pisteet');
 
 function jump() {
     dino.classList.add('animation');
-    setTimeout(function (){
-        dino.classList.remove('animate');
-    },500)
-        
+    
+    setTimeout(function() {
+       dino.classList.remove('animation');
+   },500);
 }
+   
+var checkDeath = setInterval(function(){
+    var dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+    var rockLeft = parseInt(window.getComputedStyle(rock).getPropertyValue('left'));
+    if(rockLeft<20 && rockLeft>0 && dinoTop>=130){
+        rock.style.animation = 'none';
+        rock.style.display = 'none';
 
-document.addEventListener('click', jump());
+        alert('You Lose');
+    }
+    },10);
+    
+
+   
+
+
+document.addEventListener('keypress', jump);
